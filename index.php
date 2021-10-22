05a0f5e22b4ae266e885198251ecd912a9038f25
nhannngcc19209 <nhannngcc19209@fpt.edu.vn>
2021-10-22 16:03:17 +0700

    Update index.php

--- index.php
+++ index.php
@@ -8,5 +8,32 @@
 </head>
 <body>
     <h1>hii</h1>
+    <?php
+    include_once("connec.php");
+    $id = "id";
+    $name = "name";
+    $age="age"
+    
+    // Create connection
+    $conn = pg_connect( $id, $name, $age);
+    // Check connection
+    if (!$conn) {
+        die("Connection failed: " . pg_connect_error());
+    }
+    
+    $sql = "SELECT id, name, age FROM infor";
+    $result = pg_query($conn, $sql);
+    
+    if (pg_num_rows($result) > 0) {
+        // output data of each row
+        while($row = pg_fetch_assoc($result)) {
+            echo "id: " . $row["id"]. " - Name: " . $row["name"]. " " . $row["age"]. "<br>";
+        }
+    } else {
+        echo "0 results";
+    }
+    
+    pg_close($conn);
+    ?>
 </body>
 </html>
