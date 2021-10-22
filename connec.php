a83e7b889319c6afd72b6fbcff70547a6de40699
nhannngcc19209 <nhannngcc19209@fpt.edu.vn>
2021-10-22 15:42:11 +0700

    up

--- connec.php
+++ connec.php
@@ -1,5 +1,6 @@
 <?php
-$conn = pg_connect("postgres://zflwyanlyxoizx:466e2381a7bbf16cd25ac61f105812ca1fd2f9385353625bdf4cf8ca2deee238@ec2-34-232-245-127.compute-1.amazonaws.com:5432/d2ae7jci9v4i9u");
+$conn = pg_connect("postgres://zflwyanlyxoizx:466e2381a7bbf16cd25a
+c61f105812ca1fd2f9385353625bdf4cf8ca2deee238@ec2-34-232-245-127.compute-1.amazonaws.com:5432/d2ae7jci9v4i9u");
 	echo 'Connected Successfully!!!';
 	if(!$conn)
 	{
