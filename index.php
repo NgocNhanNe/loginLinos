349a98fa0ed4a86c2eff16712c6328ed0fd5786f
nhannngcc19209 <nhannngcc19209@fpt.edu.vn>
2021-10-22 16:30:23 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    include_once("connec.php");
+    $name=pg_query($conn,"SELECT name from infor") or 
+    die(pg_error($conn));
+    echo $name;
+    ?>
+</body>
+</html>
